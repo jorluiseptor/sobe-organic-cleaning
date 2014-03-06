8c8ad4926f9cd85b518530b84246e40f189eea6b
Jorge Sepulveda <jorluiseptor@gmail.com>
2014-03-06 14:15:37 -0500

    Add footer.

---
+++ footer.php
@@ -0,0 +1,43 @@
+<footer>
+				<div class="float-right" id="rightcolumn">
+					<ul id="social">
+                        <li><a class="yelp" title="Check our reviews on Yelp" href="http://www.yelp.com/biz/sobe-organic-cleaning-washington" target="_blank"></a></li>
+						<li><a class="facebook" title="Find us on Facebook" href="http://facebook.com/sobeorganic" target="_blank"></a></li>
+						<li><a class="twitter" title="Follow us on Twitter" href="http://twitter.com/sobeorganic" target="_blank"></a></li>
+					</ul>
+					<p id="quickcontact" class="clearfix">
+						202-885-9684<br/>
+                        <small>Mon - Fri,  9a - 5p</small><br/>
+						<a href="mailto:info@sobeorganic.com">info@sobeorganic.com</a>
+					</p>
+					<p class="gbb">
+						<a href="http://www.gbb.org/business/sobe-organic-cleaning-llc/" target="_blank" title="We're certified!"></a>
+					</p>
+                    <p class="betterlife">
+                        <a href="http://www.cleanhappens.com" target="_blank" title="SOBE's official supplier."></a>
+                    </p>
+                    <p id="pledge-link">
+                    <a href="pledge.php">Our Pledge</a>
+                    
+                    </p>
+                    
+					<p class="tagline">
+						Ultimate Solution for a Cleaner Environment
+					</p>
+					<div id="footer">
+						<p>
+							<?php 
+    			$time = time () ; 
+    			//This line gets the current time off the server
+    
+    			$year= date("Y",$time); 
+    			 //This line formats it to display just the year
+    
+    			echo "Copyright " . $year;
+    			 //this line prints out the copyright date range, you need to edit 2002 to be your opening year
+    			 ?>
+							site by <a	href="http://jorluiseptor.com" target="_blank">Jorge Sepúlveda</a>
+						</p>
+					</div>
+				</div>
+			</footer>
