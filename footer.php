5cacc38ab51b90441c0cc6cf495149d88c1f5bd8
Jorge Sepulveda <jorluiseptor@gmail.com>
2014-03-10 12:38:50 -0700

    Add schema.org meta tags to service hours and reviews.

--- footer.php
+++ footer.php
@@ -1,26 +1,42 @@
-<footer>
+<footer> 
 				<div class="float-right" id="rightcolumn">
-					<ul id="social">
-                        <li><a class="yelp" title="Check our reviews on Yelp" href="http://www.yelp.com/biz/sobe-organic-cleaning-washington" target="_blank"></a></li>
-						<li><a class="facebook" title="Find us on Facebook" href="http://facebook.com/sobeorganic" target="_blank"></a></li>
-						<li><a class="twitter" title="Follow us on Twitter" href="http://twitter.com/sobeorganic" target="_blank"></a></li>
-					</ul>
-					<p id="quickcontact" class="clearfix">
-						202-885-9684<br/>
-                        <small>Mon - Fri,  9a - 5p</small><br/>
-						<a href="mailto:info@sobeorganic.com">info@sobeorganic.com</a>
-					</p>
-                    <p class="betterlife">
-                        <a href="http://www.cleanhappens.com" target="_blank" title="SOBE's official supplier."></a>
-                    </p>
-                    <p id="pledge-link">
-                    <a href="pledge.php">Our Pledge</a>
-                    
-                    </p>
-                    
-					<p class="tagline">
-						Ultimate Solution for a Cleaner Environment
-					</p>
+				
+					<div itemscope itemtype="http://schema.org/Organization">
+					  <meta itemprop="name" content= "SOBE Organic Cleaning"/>
+					  <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+					       <meta itemprop="streetAddress" content="37 FL Ave. Suite 200 NE"/>
+					       <meta itemprop="addressLocality" content="Washington"/>			
+					       <meta itemprop="addressRegion" content="DC"/>
+					       <meta itemprop="postalCode" content="20002"/>
+					  </div>
+					
+						<ul id="social">
+	                        <li><a class="yelp" title="Check our reviews on Yelp" href="http://www.yelp.com/biz/sobe-organic-cleaning-washington" target="_blank"></a></li>
+							<li><a class="facebook" title="Find us on Facebook" href="http://facebook.com/sobeorganic" target="_blank"></a></li>
+							<li><a class="twitter" title="Follow us on Twitter" href="http://twitter.com/sobeorganic" target="_blank"></a></li>
+						</ul>
+						<p id="quickcontact" class="clearfix">
+							<span itemprop="telephone">202-885-9684</span><br/>
+	                        <small>
+	                            <time itemprop="openingHours" itemscope itemtype="http://schema.org/LocalBusiness" datetime="Mo-Fr 09:00-17:00">Mon - Fri,  9a - 5p</time>
+	                        </small>
+	                        <br/>
+							<a href="mailto:info@sobeorganic.com"><span itemprop="email">info@sobeorganic.com</span></a>
+						</p>
+	                    <p class="betterlife">
+	                        <a href="http://www.cleanhappens.com" target="_blank" title="SOBE's official supplier."></a>
+	                    </p>
+	                    <p id="pledge-link">
+	                    <a href="pledge.php">Our Pledge</a>
+	                    
+	                    </p>
+	                    
+						<p class="tagline">
+							Ultimate Solution for a Cleaner Environment
+						</p>
+					</div> <!-- /organization -->
+					
+					
 					<div id="footer">
 						<p>
 							<?php 
--- header.php
+++ header.php
@@ -25,43 +25,83 @@
 						</div>
 						<div class="scrollable">
 							<div class="items">
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"I highly recommend SOBE Organic Cleaning services especially to folks like myself who enjoy coming to a speckless, clean and fresh home after a very busy work day. The cleaning personnel are always punctual, they do their work quickly yet efficiently and conscientiously. Take my word for it!" Shawn Johnson Jr.
 									</p>
 								</div>
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"We are delighted with the friendly and efficient service. We have a small child crawling around so the use of ecologically friendly cleaning products is very important to us". M.Morisson
 									</p>
 								</div>
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"A very friendly and professional team who I would gladly use again." Amy C.
 									</p>
 								</div>
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"I was amazed at how clean SOBE was able to get my home without harsh bleaches or chemicals, and the place was even aromatic!  Truly an awesome job!" Dexter G.
 									</p>
 								</div>
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"The whole process was so carefree that I now use SOBE Organic Cleaning at least twice a month in my new place!" Rene A.
 									</p>
 								</div>
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"Great service, great convenience… just plain great!" Kris M.
 									</p>
 								</div>
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"After having our new baby, we wanted to ensure a toxin-free environment for our new family. I was EXTREMELY happy with our results. Thanks." Peter B.
 									</p>
 								</div>
-								<div>
-									<p>
+								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
+									<div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
+								      <meta itemprop="worstRating" content = "1"/>
+								      <meta itemprop="ratingValue" content = "5"/>
+								      <meta itemprop="bestRating" content = "5"/>
+								    </div>
+									<p itemprop="description">
 										"Great customer service goes a long way…not to mention the feeling of coming home to an incredibly clean home after a hard day at work.  Thanks SOBE!" Monica R.
 									</p>
 								</div>
