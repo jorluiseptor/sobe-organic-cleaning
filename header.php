ac85cdf05f297985ded090ac36a6a1bcb80cfd81
Jorge Sepulveda <jorluiseptor@gmail.com>
2014-03-11 11:27:06 -0700

    Fix reviews schema tags.

--- header.php
+++ header.php
@@ -31,8 +31,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"I highly recommend SOBE Organic Cleaning services especially to folks like myself who enjoy coming to a speckless, clean and fresh home after a very busy work day. The cleaning personnel are always punctual, they do their work quickly yet efficiently and conscientiously. Take my word for it!" Shawn Johnson Jr.
+									<p><span itemprop="description">
+										"I highly recommend SOBE Organic Cleaning services especially to folks like myself who enjoy coming to a speckless, clean and fresh home after a very busy work day. The cleaning personnel are always punctual, they do their work quickly yet efficiently and conscientiously. Take my word for it!"</span><span itemprop="author"> Shawn Johnson Jr.</span>
 									</p>
 								</div>
 								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
@@ -41,8 +41,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"We are delighted with the friendly and efficient service. We have a small child crawling around so the use of ecologically friendly cleaning products is very important to us". M.Morisson
+									<p> <span itemprop="description">
+										"We are delighted with the friendly and efficient service. We have a small child crawling around so the use of ecologically friendly cleaning products is very important to us". </span><span itemprop="author">M.Morisson</span>
 									</p>
 								</div>
 								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
@@ -51,8 +51,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"A very friendly and professional team who I would gladly use again." Amy C.
+									<p><span itemprop="description">
+										"A very friendly and professional team who I would gladly use again." </span><span itemprop="author">Amy C.</span>
 									</p>
 								</div>
 								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
@@ -61,8 +61,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"I was amazed at how clean SOBE was able to get my home without harsh bleaches or chemicals, and the place was even aromatic!  Truly an awesome job!" Dexter G.
+									<p><span itemprop="description">
+										"I was amazed at how clean SOBE was able to get my home without harsh bleaches or chemicals, and the place was even aromatic!  Truly an awesome job!" </span><span itemprop="author">Dexter G.</span>
 									</p>
 								</div>
 								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
@@ -71,8 +71,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"The whole process was so carefree that I now use SOBE Organic Cleaning at least twice a month in my new place!" Rene A.
+									<p><span itemprop="description">
+										"The whole process was so carefree that I now use SOBE Organic Cleaning at least twice a month in my new place!" </span><span itemprop="author">Rene A.</span>
 									</p>
 								</div>
 								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
@@ -81,8 +81,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"Great service, great convenience… just plain great!" Kris M.
+									<p><span itemprop="description">
+										"Great service, great convenience… just plain great!" </span><span itemprop="author">Kris M.</span>
 									</p>
 								</div>
 								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
@@ -91,8 +91,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"After having our new baby, we wanted to ensure a toxin-free environment for our new family. I was EXTREMELY happy with our results. Thanks." Peter B.
+									<p><span itemprop="description">
+										"After having our new baby, we wanted to ensure a toxin-free environment for our new family. I was EXTREMELY happy with our results. Thanks."</span> <span itemprop="author">Peter B.</span>
 									</p>
 								</div>
 								<div itemprop="review" itemscope itemtype="http://schema.org/Review">
@@ -101,8 +101,8 @@
 								      <meta itemprop="ratingValue" content = "5"/>
 								      <meta itemprop="bestRating" content = "5"/>
 								    </div>
-									<p itemprop="description">
-										"Great customer service goes a long way…not to mention the feeling of coming home to an incredibly clean home after a hard day at work.  Thanks SOBE!" Monica R.
+									<p > <span itemprop="description">
+										"Great customer service goes a long way…not to mention the feeling of coming home to an incredibly clean home after a hard day at work.  Thanks SOBE!" </span><span itemprop="author">Monica R.</span>
 									</p>
 								</div>
 							</div>
